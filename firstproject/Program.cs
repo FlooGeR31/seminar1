f7fe9b23fd5e0641d6fe6f6d567ea61173d541a1
FlooGeR31 <fridrih.oushen@yandex.ru>
2023-07-23 16:04:29 +0300

    second task

--- firstproject/Program.cs
+++ firstproject/Program.cs
@@ -12,22 +12,43 @@ int sqr1 = Convert.ToInt32(Math.Pow(number, 2));
 Console.WriteLine($"Квадрат числа {number}, с применением библиотеки, равен {sqr1}");*/
 
 
-Console.Write("Введите первое число: ");
+/*Console.Write("Введите первое число: ");
 int a = int.Parse(Console.ReadLine()!);
 
 Console.Write("Введите второе число: ");
 int b = int.Parse(Console.ReadLine()!);
 
-    if (Convert.ToInt32(Math.Pow(a,2)) == b)
+if (Convert.ToInt32(Math.Pow(a, 2)) == b)
 {
     Console.Write($"Да, число {a} является квадратом числа {b}");
 }
-    else
+else
 {
-Console.WriteLine($"Нет, число {a} не является квадратом числа {b}");
+    Console.WriteLine($"Нет, число {a} не является квадратом числа {b}");
 }
+*/
 
+/*Напишите программу, которая будет выдавать название дня недели по заданному номеру.
 
+3 -> Среда
+5 -> Пятница*/
 
+Console.Write("Введите число от 1 до 7: ");
+int number = int.Parse(Console.ReadLine()!);
+int Monday = 1;
+int Tuesday = 2;
+int Wednesday = 3;
+int Thursday = 4;
+int Friday = 5;
+int Saturday = 6;
+int Sunday = 7;
 
+if (number == Monday){Console.WriteLine("Понедельник");}
+else if (number == Tuesday){Console.WriteLine("Вторник");}
+else if (number == Wednesday){ Console.WriteLine("Cреда");}
+else if (number == Thursday){Console.WriteLine("Четверг");}
+else if (number == Friday){Console.WriteLine("Пятница");}
+else if (number == Saturday){Console.WriteLine("Суббота");}
+else if (number == Sunday){ Console.WriteLine("Воскресенье");}
+else {Console.WriteLine("В неделе всего 7 дней, введи корректное число!");}
 

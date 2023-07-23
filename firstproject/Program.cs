3f0f7a40da40470d9c8dc6b541cbbe68471b052c
FlooGeR31 <fridrih.oushen@yandex.ru>
2023-07-23 15:49:24 +0300

    first task

--- firstproject/Program.cs
+++ firstproject/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.Write("Введите любое число :");
+﻿/*Console.Write("Введите любое число :");
 int number = int.Parse(Console.ReadLine()!);
 
 // Математический способ
@@ -9,5 +9,25 @@ Console.WriteLine($"Квадрат числа {number} равен {sqr}");
 // Библиотека
 
 int sqr1 = Convert.ToInt32(Math.Pow(number, 2));
-Console.WriteLine($"Квадрат числа {number}, с применением библиотеки, равен {sqr1}");
+Console.WriteLine($"Квадрат числа {number}, с применением библиотеки, равен {sqr1}");*/
+
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+    if (Convert.ToInt32(Math.Pow(a,2)) == b)
+{
+    Console.Write($"Да, число {a} является квадратом числа {b}");
+}
+    else
+{
+Console.WriteLine($"Нет, число {a} не является квадратом числа {b}");
+}
+
+
+
+
 

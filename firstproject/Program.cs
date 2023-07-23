252473359f054115ce4983bd1a5c1d300c71371f
FlooGeR31 <fridrih.oushen@yandex.ru>
2023-07-23 15:16:24 +0300

    Lib metod

--- firstproject/Program.cs
+++ firstproject/Program.cs
@@ -4,4 +4,10 @@ int number = int.Parse(Console.ReadLine()!);
 // Математический способ
 
 int sqr = number * number;
-Console.WriteLine(sqr);
+Console.WriteLine($"Квадрат числа {number} равен {sqr}");
+
+// Библиотека
+
+int sqr1 = Convert.ToInt32(Math.Pow(number, 2));
+Console.WriteLine($"Квадрат числа {number}, с применением библиотеки, равен {sqr1}");
+
